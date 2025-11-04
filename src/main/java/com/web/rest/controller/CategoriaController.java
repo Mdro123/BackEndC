62603eedba91e2ac18a817141855cd926f7514cd
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-04 11:47:24 -0500

    Update CategoriaController.java

--- src/main/java/com/web/rest/controller/CategoriaController.java
+++ src/main/java/com/web/rest/controller/CategoriaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categorias")
-@CrossOrigin(origins = "http://localhost:4200/") // Ajusta si es necesario
+@CrossOrigin(origins = "http://localhost:4200/")
 public class CategoriaController {
 
     @Autowired
