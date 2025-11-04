12c40973265637f3538b04f02db3723f147208e1
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-04 12:02:49 -0500

    Update CategoriaDTO.java

--- src/main/java/com/web/rest/dto/CategoriaDTO.java
+++ src/main/java/com/web/rest/dto/CategoriaDTO.java
@@ -27,4 +27,5 @@ public class CategoriaDTO {
     public void setNombre(String nombre) { this.nombre = nombre; }
     public String getDescripcion() { return descripcion; }
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    
 }
