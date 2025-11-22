e8b95cf34b1d54e6273053a41bea173a05728a61
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-22 12:41:43 -0500

    Create WishlistService.java

---
+++ src/main/java/com/web/rest/service/WishlistService.java
@@ -0,0 +1,10 @@
+package com.web.rest.service;
+
+import com.web.rest.model.WishlistItem;
+import java.util.List;
+
+public interface WishlistService {
+    WishlistItem addToWishlist(Integer userId, Integer productoId);
+    void removeFromWishlist(Integer userId, Integer productoId);
+    List<WishlistItem> getWishlistByUserId(Integer userId);
+}
