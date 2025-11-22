607b71a8f805a1448bf55ae72dbae928f61aaaaa
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-22 12:49:32 -0500

    Update WishlistService.java

--- src/main/java/com/web/rest/service/WishlistService.java
+++ src/main/java/com/web/rest/service/WishlistService.java
@@ -3,6 +3,7 @@ package com.web.rest.service;
 import com.web.rest.model.WishlistItem;
 import java.util.List;
 
+
 public interface WishlistService {
     WishlistItem addToWishlist(Integer userId, Integer productoId);
     void removeFromWishlist(Integer userId, Integer productoId);
