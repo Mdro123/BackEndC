9dd3a5ed54d0d0c9af36749d97e4000eecc84e72
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-22 12:40:31 -0500

    Create WishlistRepository.java

---
+++ src/main/java/com/web/rest/repository/WishlistRepository.java
@@ -0,0 +1,20 @@
+package com.web.rest.repository;
+
+import com.web.rest.model.Producto;
+import com.web.rest.model.Usuario;
+import com.web.rest.model.WishlistItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<WishlistItem, Integer> {
+    // Verifica si ya existe el libro en la lista del usuario
+    boolean existsByUsuarioAndProducto(Usuario usuario, Producto producto);
+    
+    // Borra un libro específico de la lista de un usuario
+    void deleteByUsuarioAndProducto(Usuario usuario, Producto producto);
+    
+    // Obtiene toda la lista de deseos de un usuario
+    List<WishlistItem> findByUsuario(Usuario usuario);
+}
